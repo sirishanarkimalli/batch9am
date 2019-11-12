f1e5b66b451f5216b23215e4e1786f067213cf39
91998 <91998@192.168.0.7>
2019-11-12 14:40:24 +0530

    program demonstrating Array Class Methods

--- javase/src/basics/ArraysClassDemo.java
+++ javase/src/basics/ArraysClassDemo.java
@@ -5,21 +5,30 @@ import java.util.Arrays;
 public class ArraysClassDemo {
 
 	public static void main(String[] args) {
-		int a[]= {40,50,32,88,97};
-		String names[]= {"Rahul","John","Rohit"};
-	
-		
+		int a[] = { 40, 50, 32, 88, 97 };
+		String names[] = { "Rahul", "John", "Rohit" };
+
+		System.out.println("Array elemenst before sort");
+		for (int value : a)
+			System.out.print(value + " ");
+
 		Arrays.sort(a);
-		
-		System.out.println(Arrays.binarySearch(a, 100));
+
+		System.out.println("\nArray elemenst after sort");
+		for (int value : a)
+			System.out.print(value + " ");
+
+		int index = Arrays.binarySearch(a, 88);
+		if (index < 0)
+			System.out.println("Element not found");
+		else
+			System.out.println("Element found at " + index);
+
 		Arrays.sort(names);
-		
-		for(String name:names)
-			System.out.print(name+" ");
-		
-		System.out.println();
-		for(int value :a)
-			System.out.print(value+" ");
+
+		System.out.println("\nString array after sorting\n");
+		for (String name : names)
+			System.out.print(name + " ");
 	}
 
 }
