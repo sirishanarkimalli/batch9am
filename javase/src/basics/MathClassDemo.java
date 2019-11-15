38c63e9ad5e803827c0573ffec73a10d797a53fb
91998 <91998@192.168.0.7>
2019-11-15 10:09:16 +0530

    Program explains about Math class methods

---
+++ javase/src/basics/MathClassDemo.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class MathClassDemo {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-20));
+		System.out.println(Math.pow(4,2));
+		System.out.println(Math.PI);
+		System.out.println(Math.min(234, 98));
+		System.out.println(Math.max(234, 98));
+		
+
+	}
+
+}
