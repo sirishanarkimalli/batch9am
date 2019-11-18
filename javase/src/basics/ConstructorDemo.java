ebbc325ae1984885857e130bce1aad7f7b9024f9
91998 <91998@192.168.0.8>
2019-11-18 10:45:25 +0530

    Program about constructors

--- javase/src/basics/ConstructorDemo.java
+++ javase/src/basics/ConstructorDemo.java
@@ -2,43 +2,50 @@ package basics;
 
 class Course1 {
 
-	String name;
-	int duration;
-	double cost;
-	
+	private String name;
+	private int duration;
+	private double cost;
+
 	public static String instituteName = "Sunshine";
 	
 	//default constructor
-	 Course1() {
-		name=null;
-		duration=0;
-		cost=0.0;
-	 }
-	
-	 //Parameterized Constructor
-	Course1(String n, int d, double c){
-		name=n;
-		duration =d;
-		cost =c;
+	Course1(){
+		this(null,0,0.0);
 	}
 
+	// Parameterized Constructor
+	Course1(String name, int duration, double cost) {
+		this.name = name;
+		this.duration = duration;
+		this.cost = cost;
+
+	}
+	
+	public Course1 getObject() {
+		return this;		
+	}
+	
 	public void displayDetails() {
+		System.out.println(this.toString());
 		System.out.println("Name= " + name);
 		System.out.println("Duration= " + duration);
 		System.out.println("Cost= " + cost);
-		System.out.println("Institute=" + Course.instituteName+"\n");
+		System.out.println("Institute=" + Course.instituteName + "\n");
 	}
 }
 
 public class ConstructorDemo {
 
 	public static void main(String[] args) {
-		Course1 c1 = new Course1("C",25,2500);
-		Course1 c2 = new Course1("Java",35,4000);
-		Course1 c3 = new Course1("Adv Java",45,5000);
+		Course1 c1 = new Course1("C", 25, 2500);
+		Course1 c2 = new Course1("Java", 35, 4000);
+		Course1 c3 = new Course1("Adv Java", 45, 5000);
+		System.out.println(c1.toString());
 		
+		
+
 		Course1 obj = new Course1();
-		
+
 		c1.displayDetails();
 		c2.displayDetails();
 		c3.displayDetails();
