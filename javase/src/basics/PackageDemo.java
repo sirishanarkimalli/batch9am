e082d4db767ca3d5897ced68bdb71ee800a14869
91998 <91998@192.168.0.8>
2019-11-21 10:03:34 +0530

    Program to illustrate how to import class from another package

---
+++ javase/src/basics/PackageDemo.java
@@ -0,0 +1,13 @@
+package basics;
+
+import hr.Employee;
+import static java.lang.Math.PI;
+import static java.lang.System.out;
+
+public class PackageDemo {
+
+	public static void main(String args[]) {
+		Employee e = new Employee();
+		out.println("Pi=" + PI);
+	}
+}
