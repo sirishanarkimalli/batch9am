31596d7fcc2d38d4440f1b584a8b62033e5b2e8d
91998 <91998@192.168.0.2>
2019-12-02 10:11:15 +0530

    Employee class overriding compareTo method

--- javase/src/hr/Employee.java
+++ javase/src/hr/Employee.java
@@ -1,5 +1,60 @@
 package hr;
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
+	int empno;
+	String ename;
+	double salary;
+	
+	public Employee() {
+	
+	}
+	
+	public Employee(int empno,String ename,double salary) {
+		this.empno=empno;
+		this.ename=ename;
+		this.salary=salary;
+	}
 
+	public int getEmpNo() {
+		return empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Employee emp=(Employee) obj;
+		if(  ( 
+				(this.ename.equals(emp.ename))& 
+				(this.empno==emp.empno)
+			 )	&
+				(this.salary==emp.salary) ){
+			return true;
+
+		}
+					else return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return empno;
+	}
+	
+	@Override
+	public String toString() {
+		return empno+" "+ename;
+	}
+	
+	@Override
+	public int compareTo(Employee e) {
+		if(this.ename.equals(e.ename))
+				return (this.empno-e.empno);
+	else  return this.ename.compareTo(e.ename);
+	}
 }
